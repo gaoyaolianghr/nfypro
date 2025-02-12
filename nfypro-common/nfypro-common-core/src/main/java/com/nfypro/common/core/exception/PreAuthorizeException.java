beba959ab210b3ee5ae69bba7b0af48e5d30e2c7
gaoyl <634245288@qq.com>
2025-02-12 11:22:42 +0800

    common

---
+++ nfypro-common/nfypro-common-core/src/main/java/com/nfypro/common/core/exception/PreAuthorizeException.java
@@ -0,0 +1,15 @@
+package com.nfypro.common.core.exception;
+
+/**
+ * 权限异常
+ * 
+ * @author ruoyi
+ */
+public class PreAuthorizeException extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+
+    public PreAuthorizeException()
+    {
+    }
+}
